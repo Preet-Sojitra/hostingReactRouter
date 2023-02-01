b0decba59eff84cbac426e111cd18ef76eec7e5c
Preet Sojitra <preet.dev373@gmail.com>
2023-02-01 12:19:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Components/Home'
+import About from "./Components/About"
+import Contact from "./Components/Contact"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About/>} />
+        <Route path='contact' element={<Contact/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
